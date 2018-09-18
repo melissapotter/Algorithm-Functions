a41101c99ff130d86ebe30fb8d84b47bf6ea63df
Melissa <melissapotter.student@careerdevs.com>
2018-09-18 23:52:33 +0000

    9-18

---
+++ encrypt.js
@@ -0,0 +1,15 @@
+function encrypt(firstInitial, lastInitial, message) {
+    var alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var letterkey =[];
+    for(let i = 0; i < 27; i++){
+        letterkey[alphabet[i]] = i;
+    }
+    for(let j=0; j < message.length; j++){
+        let sum = letterkey[firstInitial] + letterkey[lastInitial];
+        let total = letterkey[message[j]] * sum;
+        console.log(total);
+    }
+    
+}
+
+encrypt("r", "f", "this is not my dog");
