ab77b534f316d1e6b24e89a75663b4e742759b36
Melissa <melissapotter.student@careerdevs.com>
2018-09-26 00:00:30 +0000

    for loops

---
+++ loop.js
@@ -0,0 +1,5 @@
+var cohortFiveNames = ["Paul", "Dan", "Brett", "Josh", "Tom", "Christian", "Joe", "Mick", "Melissa", "Nashan", "Alejandra"];
+for (var i = 0; i < cohortFiveNames.length; i++) {
+   console.log("Hello " + cohortFiveNames[i]);
+}
+// first commit
